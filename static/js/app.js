5befbcf34ddce893ecb1bf14d2fe23c95ac740a6
Hidde Statema <hiddestatema@me.com>
2013-10-07 13:07:19 +0200

    asdf
    asdf

--- static/js/app.js
+++ static/js/app.js
@@ -2,93 +2,88 @@ var APP = APP || {};
 
 (function () {
 	"use strict";
+
+	APP.settings = {
+		networkPath: "https://api.leaguevine.com/v1/"
+	};
 	
 	// Schedule pagina data
-	APP.schedule = {
-		scheduleArray: [
-			{ date: "Monday, 9:00am", team1: "Chasing", team1Score: "13", team2: "Amsterdam Money Gang", team2Score: "9"},
-			{ date: "Monday, 9:00am", team1: "Boomsquad", team1Score: "15", team2: "Beast Amsterdam", team2Score: "11"},
-			{ date: "Monday, 10:00am", team1: "Beast Amsterdam", team1Score: "14", team2: "Amsterdam Money Gang", team2Score: "12"},
-			{ date: "Monday, 10:00am", team1: "Chasing", team1Score: "5", team2: "Burning Snow", team2Score: "15"},
-			{ date: "Monday, 11:00am", team1: "Boomsquad", team1Score: "11", team2: "Amsterdam Money Gang", team2Score: "15"},    
-			{ date: "Monday, 11:00am", team1: "Burning Snow", team1Score: "15", team2: "Beast Amsterdam", team2Score: "6"},
-			{ date: "Monday, 12:00pm", team1: "Chasing", team1Score: "8", team2: "Beast Amsterdam", team2Score: "15"},
-			{ date: "Monday, 12:00pm", team1: "Boomsquad", team1Score: "15", team2: "Burning Snow", team2Score: "8"},
-			{ date: "Monday, 1:00pm", team1: "Chasing", team1Score: "15", team2: "Boomsquad", team2Score: "14"},
-			{ date: "Monday, 1:00pm", team1: "Burning Snow", team1Score: "15", team2: "Amsterdam Money Gang", team2Score: "11"}
-		],
+	APP.teams = {
+		teamsArray: [ ],
+		page: 0,
+		limit: 5,
 
-		// Order functie voor het ordenen van de data, winnaars staan altijd links
-		orderArray: function () {
+		getTeamsFromLeagueVine: function (page) {
+			var self = this;
 
-			for(var i = 0; i < APP.schedule.scheduleArray.length; i++) {
-				// Van string naar float, zodat je berekeningen er mee kan doen
-				var score1 = parseFloat(APP.schedule.scheduleArray[i].team1Score),
-					score2 = parseFloat(APP.schedule.scheduleArray[i].team2Score);
-				
-				// If winnaar staat rechts, verplaats naar links	
-				if(score1 < score2) {
-					var tempScore1 = score1,
-						tempScore2 = score2,
-						tempTeam1 = APP.schedule.scheduleArray[i].team1,
-						tempTeam2 = APP.schedule.scheduleArray[i].team2;
-					
-					// Change scores
-					APP.schedule.scheduleArray[i].team1Score = tempScore2;
-					APP.schedule.scheduleArray[i].team2Score = tempScore1;
-					
-					// Change teamnames
-					APP.schedule.scheduleArray[i].team1 = tempTeam2;
-					APP.schedule.scheduleArray[i].team2 = tempTeam1;
-				}
+			var offset = page * this.limit;
+			var httpLink = APP.settings.networkPath + 'tournament_teams/?tournament_ids=%5B19389%5D&order_by=%5Bseed%5D&limit=' + this.limit + '&offset=' + offset + '&access_token=6dd70849a7';
+
+			this.page = page;
+			
+			promise.get(httpLink).then(function(error, text, xhr) {
+			    if (error) {
+			      console.log('Error ' + xhr.status);
+			      return;
+			    }
+
+			    self.teamsArray = [];
+
+			    var data = JSON.parse(text);
+
+			    for(var i = 0; i < data.objects.length; i++) {
+			    	APP.teams.teamsArray[i] = {
+			    		id: data.objects[i].team_id,
+			    		name: data.objects[i].team.name,
+			    		seed: data.objects[i].seed,
+			    	};
+			    }
+
+			    APP.page.render("teams");
+			});
+		},
+
+		nextPage: function(){
+			this.page++;
+
+			this.getTeamsFromLeagueVine(this.page);			
+		},
+
+		previousPage: function(){
+			if(this.page >= 1) {
+				this.page--;
+
+				this.getTeamsFromLeagueVine(this.page);			
 			}
 		}
 	};
 
-	// Game pagina data
-	APP.game = {
-		gameArray: [
-		    { score: "1", team1: "Boomsquad", team1Score: "1", team2: "Burning Snow", team2Score: "0"},
-		    { score: "2", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "0"},
-		    { score: "3", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "1"},
-		    { score: "4", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "2"},
-		    { score: "5", team1: "Boomsquad", team1Score: "3", team2: "Burning Snow", team2Score: "2"},
-		    { score: "6", team1: "Boomsquad", team1Score: "4", team2: "Burning Snow", team2Score: "2"},
-		    { score: "7", team1: "Boomsquad", team1Score: "5", team2: "Burning Snow", team2Score: "2"},
-		    { score: "8", team1: "Boomsquad", team1Score: "5", team2: "Burning Snow", team2Score: "3"},
-		    { score: "9", team1: "Boomsquad", team1Score: "6", team2: "Burning Snow", team2Score: "3"},
-		    { score: "10", team1: "Boomsquad", team1Score: "7", team2: "Burning Snow", team2Score: "3"},
-		    { score: "11", team1: "Boomsquad", team1Score: "7", team2: "Burning Snow", team2Score: "4"},
-		    { score: "12", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "4"},
-		    { score: "13", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "5"},
-		    { score: "14", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "6"},
-		    { score: "15", team1: "Boomsquad", team1Score: "9", team2: "Burning Snow", team2Score: "6"},
-		    { score: "16", team1: "Boomsquad", team1Score: "9", team2: "Burning Snow", team2Score: "7"},
-		    { score: "17", team1: "Boomsquad", team1Score: "10", team2: "Burning Snow", team2Score: "7"},
-		    { score: "18", team1: "Boomsquad", team1Score: "11", team2: "Burning Snow", team2Score: "7"},
-		    { score: "19", team1: "Boomsquad", team1Score: "12", team2: "Burning Snow", team2Score: "7"},
-		    { score: "20", team1: "Boomsquad", team1Score: "13", team2: "Burning Snow", team2Score: "7"},
-		    { score: "21", team1: "Boomsquad", team1Score: "14", team2: "Burning Snow", team2Score: "7"},
-		    { score: "22", team1: "Boomsquad", team1Score: "14", team2: "Burning Snow", team2Score: "8"},
-		    { score: "23", team1: "Boomsquad", team1Score: "15", team2: "Burning Snow", team2Score: "8"}
-		]
-	};
+	APP.pools = {
+		poolsArray: [],
 
-	// Ranking pagina data
-	APP.ranking = {
-		rankingArray: [
-		    { team: "Chasing", Win: "2", Lost: "2", Sw: "7", Sl: "9", Pw: "35", Pl: "39"},
-		    { team: "Boomsquad", Win: "2", Lost: "2", Sw: "9", Sl: "8", Pw: "36", Pl: "34"},
-		    { team: "Burning Snow", Win: "3", Lost: "1", Sw: "11", Sl: "4", Pw: "36", Pl: "23"},
-		    { team: "Beast Amsterdam", Win: "2", Lost: "2", Sw: "6", Sl: "8", Pw: "30", Pl: "34"},
-		    { team: "Amsterdam Money Gang", Win: "1", Lost: "3", Sw: "6", Sl: "10", Pw: "30", Pl: "37"}
-		]
-	};
+		getPoolsFromLeagueVine: function () {
+			var self = this;
+			
+			promise.get(APP.settings.networkPath + 'pools/?tournament_id=19389&order_by=%5Bname%5D&access_token=6dd70849a7').then(function(error, text, xhr) {
+			    if (error) {
+			      console.log('Error ' + xhr.status);
+			      return;
+			    }
 
-	// Film pagina data
-	APP.movies = {
-		details: "Bored? Watch a movie!",
-		moviesArray: []
+			    self.poolsArray = [];
+
+			    var data = JSON.parse(text);
+
+			    for(var i = 0; i < data.objects.length; i++) {
+			    	self.poolsArray[i] = {
+			    		name: data.objects[i].name
+			    	};
+			    }
+
+			    APP.page.render("pools");
+			});
+		}
+
 	};
 	
 	// Controller 
@@ -96,37 +91,37 @@ var APP = APP || {};
 		init: function () {
 			// Initialize router
 			APP.router.init();
-
-			// Order de array
-			APP.schedule.orderArray();
-
-			// Request movies
-			APP.controller.getMoviesArray();
-		},
-
-		getMoviesArray: function () {
-			
-			promise.get('http://dennistel.nl/movies').then(function(error, text, xhr) {
-			    if (error) {
-			      console.log('Error ' + xhr.status);
-			      return;
-			    }
-
-			    APP.movies = JSON.parse(text);
-
-			    Transparency.render(qwery('[data-route=movies')[0], APP.movies);
-			    console.log('The page contains ' + text.length + ' character(s).');
-			    console.log(text)
-			});
-
 		}
 	};
 
 	// Router
 	APP.router = {
 		init: function () {
-			routie('/:name', function(name) {
+			/*routie('/:name', function(name) {
 			    APP.page.render(name);
+			});*/
+
+			routie({
+			    '/teams': function() {
+			    	APP.teams.getTeamsFromLeagueVine(0);
+			    	APP.page.render("teams");
+			    },
+
+			    '*': function() {
+			    	APP.teams.getTeamsFromLeagueVine(0);
+			    	APP.page.render("teams");
+			    },
+
+			    '/teams/:page': function(page) {
+			    	APP.teams.getTeamsFromLeagueVine(page);
+			    	APP.page.render("teams");
+			    },
+
+
+			    '/pools': function() {
+			    	APP.pools.getPoolsFromLeagueVine();
+			    	APP.page.render("pools");
+			    }
 			});
 		},
 
